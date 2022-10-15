a7d1536c36e7df2f73410e578ff46a13c32382aa
Brent Keller <bikeller104@gmail.com>
2022-10-15 11:11:04 -0600

    added the current date to the header using dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 console.log("script has been imported correctly");
 
 //test that day.js was imported correctly
-console.log(dayjs().get('year'));
+console.log(dayjs()/*.get('year')*/);
 //get the first element with the class container
 /*
 *  TODO maybe make this a main element instead of div
@@ -15,9 +15,13 @@ console.log(timeContainer);
 
 
 var header = $(".jumbotron");
-
 console.log(header);
 
+//get the current date element and add
+//the dayjs current date to it formatted correctly
+var curDay = $("#currentDay")
+//curDay.text("date goes here");
+curDay.text(getCurrentDate());
 
 
 
@@ -25,3 +29,19 @@ console.log(header);
 
 
 
+
+/*
+this function uses the dayjs() function with no parameters
+to get the current data then uses the format function to
+format it with the day name the month name and the 
+two digit day of the month
+
+https://day.js.org/docs/en/display/format
+
+*/
+function getCurrentDate()
+{
+    var todaysDate = dayjs().format('dddd MMMM DD');
+    console.log($`todays date is {todaysDate}`);
+    return todaysDate;
+}
