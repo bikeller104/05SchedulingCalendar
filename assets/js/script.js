bd128d742299d823b7d6a09da5dc3fb98f9f660a
Brent Keller <bikeller104@gmail.com>
2022-10-15 10:51:39 -0600

    tested that day.js was imported correctly and started initializing variables for HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
+//make sure that the script is incorporated into the html correctly
 console.log("script has been imported correctly");
+
+//test that day.js was imported correctly
+console.log(dayjs().get('year'));
+//get the first element with the class container
+/*
+*  TODO maybe make this a main element instead of div
+*  so i can select it easier???
+*/
+var timeContainer = $(".container")[0];
+
+//got the correct container
+console.log(timeContainer);
+
+
+var header = $(".jumbotron");
+
+console.log(header);
+
+
+
+
+
+
+
+
